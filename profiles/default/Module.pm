2182126624ecd449f607d7a58b004532da08eb36
HIROSE Masaaki <hirose31@gmail.com>
2013-11-28 12:54:28 +0900

    Remove duplicated SEE ALSO

--- profiles/default/Module.pm
+++ profiles/default/Module.pm
@@ -131,6 +131,4 @@ Copyright {{(my $a = $dist->authors->[0]) =~ s/\s*<.*$//; $a}}
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
-=head1 SEE ALSO
-
 =cut
