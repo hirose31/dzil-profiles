f410dabd24efe24eabb9894b3c5980b9e57be858
HIROSE Masaaki <hirose31@gmail.com>
2014-02-06 19:28:34 +0900

    Remove local variables

--- profiles/default/skel/t/Util.pm
+++ profiles/default/skel/t/Util.pm
@@ -23,15 +23,3 @@ sub p($) {
 1;
 
 __END__
-
-# for Emacsen
-# Local Variables:
-# mode: cperl
-# cperl-indent-level: 4
-# cperl-close-paren-offset: -4
-# cperl-indent-parens-as-block: t
-# indent-tabs-mode: nil
-# coding: utf-8
-# End:
-
-# vi: set ts=4 sw=4 sts=0 et ft=perl fenc=utf-8 :
